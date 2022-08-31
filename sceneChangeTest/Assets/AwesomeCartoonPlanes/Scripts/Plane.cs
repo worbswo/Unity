56280cc49038b40ddf49d9282041f99c622de0f3
kim jae gyoun <worbswo@naver.com>
2022-08-31 22:57:04 +0900

    add arrow key input

--- sceneChangeTest/Assets/AwesomeCartoonPlanes/Scripts/Plane.cs
+++ sceneChangeTest/Assets/AwesomeCartoonPlanes/Scripts/Plane.cs
@@ -6,13 +6,25 @@ using System.Collections;
 public class Plane : MonoBehaviour {
 	public GameObject prop;
 	public GameObject propBlured;
-
+	public int moveSpeed = 15;
 	public bool engenOn;
 	void Awake(){
 		DontDestroyOnLoad(gameObject);
 	}
 	void Update () 
 	{
+		if(Input.GetKey(KeyCode.UpArrow)){
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.DownArrow)){
+			transform.Translate(Vector3.forward*-moveSpeed*Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.RightArrow)){
+			transform.Translate(Vector3.right*moveSpeed*Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.Translate(Vector3.left*moveSpeed*Time.deltaTime);
+		}
 		if (engenOn) {
 			prop.SetActive (false);
 			propBlured.SetActive (true);
