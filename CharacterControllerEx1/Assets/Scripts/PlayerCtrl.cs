a7f7dec6f2b6dd58d956cf18f09276deb921b00c
kim jae gyoun <worbswo@naver.com>
2022-09-07 22:47:22 +0900

    add animation trigger

--- CharacterControllerEx1/Assets/Scripts/PlayerCtrl.cs
+++ CharacterControllerEx1/Assets/Scripts/PlayerCtrl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerCtrl : MonoBehaviour
 {
     CharacterController controller;
+    Animator animator;
     Vector3 moveDir = Vector3.zero;
     int speed =3;
     int rotation_speed = 2;
@@ -14,6 +15,7 @@ public class PlayerCtrl : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,6 +24,19 @@ public class PlayerCtrl : MonoBehaviour
         moveDir.z= Input.GetAxis("Vertical") *speed;
         transform.Rotate(0,Input.GetAxis("Horizontal")*rotation_speed,0);
         Vector3 globalDir = transform.TransformDirection(moveDir);
+        if(!animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.run")){
+            if(Input.GetAxis("Horizontal")>0 || Input.GetAxis("Horizontal")<0){
+                animator.SetTrigger("Run");
+            }
+        }
+        if(!animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.run")){
+            if(moveDir.z>0 || moveDir.z<0){
+                animator.SetTrigger("Run");
+            }
+        }
+        if(Input.GetAxis("Horizontal")==0 && Input.GetAxis("Vertical")==0){
+                animator.SetTrigger("Idle");
+            }
         if(controller.isGrounded){
             if(Input.GetKey(KeyCode.Space)){
                 gravity = 10.0f;
