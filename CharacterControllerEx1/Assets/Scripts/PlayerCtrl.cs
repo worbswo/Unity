2a9a7731b75254f7a1987c51af3916876cf6e769
kim jae gyoun <worbswo@naver.com>
2022-09-07 22:03:41 +0900

    add jump key in PlayerCtrl.cs

--- CharacterControllerEx1/Assets/Scripts/PlayerCtrl.cs
+++ CharacterControllerEx1/Assets/Scripts/PlayerCtrl.cs
@@ -8,6 +8,8 @@ public class PlayerCtrl : MonoBehaviour
     Vector3 moveDir = Vector3.zero;
     int speed =3;
     int rotation_speed = 2;
+    float gravity = 0.0f;
+    float jumpSpeed = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +22,22 @@ public class PlayerCtrl : MonoBehaviour
         moveDir.z= Input.GetAxis("Vertical") *speed;
         transform.Rotate(0,Input.GetAxis("Horizontal")*rotation_speed,0);
         Vector3 globalDir = transform.TransformDirection(moveDir);
-        controller.Move(moveDir * Time.deltaTime);
+        if(controller.isGrounded){
+            if(Input.GetKey(KeyCode.Space)){
+                gravity = 10.0f;
+                moveDir.y=jumpSpeed;
+                Debug.Log("1\n");
+            }
+        }
+        if(moveDir.y<=0.0f){
+            moveDir.y=0.0f;
+            gravity =0.0f;
+        }
+        else{
+            gravity-=0.4f;
+        }
+
+        moveDir.y+= gravity*Time.deltaTime;
+        controller.Move(globalDir*Time.deltaTime);
     }
 }
