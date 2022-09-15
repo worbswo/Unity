06ba3494f87eed46b5ca333c35aab0df0226a6b8
kim jae gyoun <worbswo@naver.com>
2022-09-15 22:30:38 +0900

    add Timer check

--- RatcastEx1/Assets/Scripts/GameManager.cs
+++ RatcastEx1/Assets/Scripts/GameManager.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public Transform randPos;
     public GameObject fightingCar;
     int numOfCar = 15;
-    
+    GameObject timeTextObj;
+    int startTime;
+    int lapsedTime;
+    Text lapsedTimeText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +20,15 @@ public class GameManager : MonoBehaviour
             randPos.position = new Vector3(Random.Range(-7f,7f),0,Random.Range(28f,32f));
             Instantiate(fightingCar,randPos.position,randPos.rotation);
         }    
+        timeTextObj = GameObject.Find("Timer");
+        lapsedTimeText=timeTextObj.GetComponent<Text>();
+        startTime = (int)Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        lapsedTime = (int)Time.time-startTime;
+        lapsedTimeText.text = "지나간 시간: " + lapsedTime.ToString();
     }
 }
