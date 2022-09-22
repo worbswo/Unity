a317e10a105755b729832195507bd6accc55dbfa
kim jae gyoun <worbswo@naver.com>
2022-09-22 21:58:38 +0900

    add Player.cs and Player object

---
+++ DelegateRedAlert/Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public delegate void PlayerEventHandler();
+
+    public static PlayerEventHandler OnPlayerEvent;
+    int health = 50;
+    public float speed =3f;
+    public int Health{
+        get{return health;}
+        set{
+            health = Mathf.Clamp(value,0,100);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space")){
+            Health = Health-5;
+            if(Health<=0){
+                OnPlayerEvent();
+            }
+            PlayerMove();
+        }
+    }
+    void PlayerMove(){
+        if(Input.GetKey(KeyCode.UpArrow)){
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.DownArrow)){
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.RightArrow)){
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+    }
+    void OnTriggerEnter(Collider other){
+        if(other.CompareTag("CHECKPOINT")){
+            OnPlayerEvent();
+        }
+    }
+}
