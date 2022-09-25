29adc61bbeceb0ae6bc9d74c8a75fe2f2b58c327
kim jae gyoun <worbswo@naver.com>
2022-09-25 22:22:53 +0900

    modify Enemy.cs for GameManager

--- DelegateRedAlert/Assets/Scripts/Enemy.cs
+++ DelegateRedAlert/Assets/Scripts/Enemy.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private void OnEnable(){
-        Player.OnPlayerEvent +=this.OnPlayerDetection;
-    }
-    private void OnDisable(){
-        Player.OnPlayerEvent-=this.OnPlayerDetection;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.instance.AddListener(GameManager.EventType.Touch,OnPlayerDetection);
+        GameManager.instance.AddListener(GameManager.EventType.Die, OnPlayerDie);
+        GameManager.instance.AddListener(GameManager.EventType.EcitTouch, onPlayerExitDetection);
     }
     void OnPlayerDetection(){
         Debug.Log("Red Alert");
         GetComponent<MeshRenderer>().material.color = Color.red;
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    void OnPlayerDie(){
+        Debug.Log("Player Dead");
+        GetComponent<MeshRenderer>().material.color = Color.yellow;
     }
-
+    void onPlayerExitDetection(){
+        Debug.Log("Exit Detection");
+        GetComponent<MeshRenderer>().material.color = Color.green;
+    }
     // Update is called once per frame
     void Update()
     {
