9a71f16fe4f16a8d7e1608c9303d44f678606dd3
kim jae gyoun <worbswo@naver.com>
2022-09-29 22:42:29 +0900

    modify HeroMove for animator

--- 3DGame/Assets/Scripts/HeroMove.cs
+++ 3DGame/Assets/Scripts/HeroMove.cs
@@ -6,6 +6,7 @@ public class HeroMove : MonoBehaviour
 {
     float h,v;
     float speed = 3.0f;
+    Animator mAvatar;
     public void onTouchValueChanged(Vector2 stickPos){
         h =stickPos.x;
         v = stickPos.y;
@@ -13,12 +14,13 @@ public class HeroMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        mAvatar = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        mAvatar.SetFloat("Speed",(h*h+v*v));
         if(h!=0f&&v!=0){
             transform.Rotate(0,h,0);
             transform.Translate(0,0,v*speed*Time.deltaTime);
